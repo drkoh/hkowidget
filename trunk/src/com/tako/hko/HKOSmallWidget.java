e694d81d0e8e06d670287a5c6ae9108cfe5f31b0
bpsauce <bpsauce@7391cd8e-1b5d-d7de-824a-06efcd775b3e>
2010-09-24 14:01:22 +0000

    Cleaner version

--- trunk/src/com/tako/hko/HKOSmallWidget.java
+++ trunk/src/com/tako/hko/HKOSmallWidget.java
@@ -1,28 +1,13 @@
 package com.tako.hko;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
-import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.PowerManager;
 import android.util.Log;
-import android.view.View;
-import android.widget.RemoteViews;
 
 public class HKOSmallWidget extends HKOWidget {
 	private static final String PREFS_NAME = "HKOPrefs";
